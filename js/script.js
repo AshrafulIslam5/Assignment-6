c6654848ec78075540c9cf6bc471b39daea99313
Mihad <ashraful.islam.mihad.com>
2022-03-01 21:09:06 +0600

    able to show products and refresh after every search Done

--- js/script.js
+++ js/script.js
@@ -14,14 +14,41 @@ function searchPhones() {
         searchResult.appendChild(div);
     }
     else {
+        searchResult.textContent = '';
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
         // console.log(url);
         fetch(url)
             .then(res => res.json())
-            .then(data => console.log(data.data))
+            .then(data => displayPhones(data.data))
     }
-};
+}
 
+const displayPhones = phones => {
+    // console.log(phones);
 
+    phones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        // div.classList.add('');
+        div.classList.add('radius-9');
+        div.classList.add('text-dark');
+        div.classList.add('shadow-lg');
+        div.classList.add('p-4');
+        div.innerHTML = `
+            <div class="text-center">
+                <img class="w-50 h-auto mb-3" src="${phone.image}">
+                <p><strong>Phone Brand: </strong> ${phone.brand}</p>
+                <p><strong>Phone name: </strong> ${phone.phone_name}</p>
+                <button class="btn btn-primary">Details</button>
+            </div>
+        
+        `;
 
-// document.getElementById('search-button').addEventListener('click', getPhones()) ;
+        const parentDiv = document.createElement('div');
+        parentDiv.classList.add('col-md-4')
+        parentDiv.classList.add('p-4')
+        parentDiv.appendChild(div);
+        searchResult.appendChild(parentDiv);
+    })
+}
+
